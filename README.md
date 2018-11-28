b8f93b28fb7f3025f6fc8a0b406f8cd732201b27
Gustavo Sales Santos <gssantos@uolinc.com>
2018-11-28 16:30:00 -0200

    aliases git

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# my-settings
-My settings of vscode, git
+# My settings
+
+The settings how I use on my developing ecosystem. Include settings of vscode and git.
+
+## Git Aliases
+
+### Install
+```bash
+$ sh git-aliases.sh
+```
+
+### Aliases
+
+#### git checkout "branch" 
+```bash
+$ git co feature/branch-name
+```
+
+#### git checkout master
+```bash
+$ git master
+```
+
+#### git branch
+```bash
+$ git br
+```
+
+#### git commit -m "message"
+```bash
+$ git ci -m "message"
+```
+
+#### git status
+```bash
+git st
+```
+
+#### git log -1 HEAD
+```bash
+git last
+```
+
+#### git log -1 reset HEAD -- file.js
+```bash
+git unstage file.js
+```
---
+++ git-aliases.sh
@@ -0,0 +1,30 @@
+#bash
+
+echo "Creating git aliases..."
+
+git config --global alias.co checkout
+echo "git co: created"
+
+git config --global alias.master 'checkout master'
+echo "git master: created"
+
+git config --global alias.br branch
+echo "git br: created"
+
+git config --global alias.ci commit
+echo "git ci: created"
+
+git config --global alias.st status
+echo "git st: created"
+
+git config --global alias.last 'log -1 HEAD'
+echo "git last: created"
+
+git config --global alias.unstage 'reset HEAD --'
+echo "git unstage: created"
+
+git config --global alias.aa 'add .'
+echo "git aa: created"
+
+git config --global alias.po 'push origin --'
+echo "git po: created"
